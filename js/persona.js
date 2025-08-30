f3c6dca6b7e065233ced78657f691d7d58e0ec0b
Carlos Yepes <carlosyepes021@gmail.com>
2025-08-30 11:29:30 -0500

    First commit

---
+++ js/persona.js
@@ -0,0 +1,26 @@
+//Las clases siempre empiezan en Mayusculas. SIEMPRE
+
+class Persona{
+    constructor(identificacion, apellidos, nombres, edad, genero,estadoCivil){
+        this.identificacion = identificacion
+        this.apellido = apellidos
+        this.nombre = nombres
+        this.edad = edad
+        this.genero = genero
+        this.estadoCivil = estadoCivil
+    }
+    imprimirDatos(){
+        console.log("++ Datos de la persons ++");
+        console.log(`Identificacion --> ${this.identificacion}`);
+        console.log(`Apellido --> ${this.apellido}`);
+        console.log(`Nombres --> ${this.nombre}`);
+        console.log(`Edad --> ${this.edad}`);
+        console.log(`Genero --> ${this.genero}`);
+        console.log(`Estado civil --> ${this.estadoCivil}`);
+        console.log(`---------------------------------------> ${}`);
+    }
+    cambiarEdad(nuevaEdad){
+        this.edad=nuevaEdad
+
+    }
+}
